16c4d648215c8c74296d5cd1be3eed8f52f31895
Zsailer <zachsailer@gmail.com>
2019-02-05 11:50:30 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Why (Pandas) DataFrames? 
+
+A lecture on why DataFrames were created. The target audience is Bioinformatics Masters' students. 
+
+I originally gave this lecture at the University of Oregon's [BGMP](https://bioinformatics.uoregon.edu/).
---
+++ utils.py
@@ -0,0 +1,66 @@
+from __future__ import print_function
+from sys import getsizeof, stderr
+from itertools import chain
+from collections import deque
+try:
+    from reprlib import repr
+except ImportError:
+    pass
+
+def total_size(o, handlers={}, verbose=False):
+    """ Returns the approximate memory footprint an object and all of its contents.
+
+    Automatically finds the contents of the following builtin containers and
+    their subclasses:  tuple, list, deque, dict, set and frozenset.
+    To search other containers, add handlers to iterate over their contents:
+
+        handlers = {SomeContainerClass: iter,
+                    OtherContainerClass: OtherContainerClass.get_elements}
+
+
+    taken from : code.activestate.com/recipes/c77504/
+    """
+    dict_handler = lambda d: chain.from_iterable(d.items())
+    all_handlers = {tuple: iter,
+                    list: iter,
+                    deque: iter,
+                    dict: dict_handler,
+                    set: iter,
+                    frozenset: iter,
+                   }
+    all_handlers.update(handlers)     # user handlers take precedence
+    seen = set()                      # track which object id's have already been seen
+    default_size = getsizeof(0)       # estimate sizeof object without __sizeof__
+
+    def sizeof(o):
+        if id(o) in seen:       # do not double count the same object
+            return 0
+        seen.add(id(o))
+        s = getsizeof(o, default_size)
+
+        if verbose:
+            print(s, type(o), repr(o), file=stderr)
+
+        for typ, handler in all_handlers.items():
+            if isinstance(o, typ):
+                s += sum(map(sizeof, handler(o)))
+                break
+        return s
+
+    return sizeof(o)
+
+
+def print_sizeof(obj):
+    size = total_size(obj)
+    print("{:,} bytes".format(size))
+
+def timeit(f):
+    def call(*args, **kwargs):
+        import time 
+        
+        start = time.time()
+        out = f(*args, **kwargs)
+        stop = time.time()
+        print("{} took: {:.10f} seconds".format(f.__name__, stop-start))
+        return out
+    return call
